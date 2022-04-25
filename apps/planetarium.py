1c3e43b08db81e5c36a866c7e693f59abf704669
Vlados <Dreshik>
2022-04-25 10:01:53 +0700

    news: добавлен вывовод новостей по команде 'последние новости'
    По умолчанию выводятся последние 3 новости с сайта.

--- apps/planetarium.py
+++ apps/planetarium.py
@@ -1,5 +1,9 @@
+from utilities.site.news import get_news_from_cache
+
+
 def get_planetarium_news():
-    return 'news'
+    news_all = get_news_from_cache()
+    return news_all
 
 
 def get_planetarium_contacts():
